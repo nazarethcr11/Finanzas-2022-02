32238c92598a6d986bb717d6459abd2083d5e450
Silene Nazareth Casma Rubio <u20201a213@upc.edu.pe>
2022-11-20 12:18:19 -0500

    fixing cositas

--- src/app/modules/timeline/timeline.module.ts
+++ src/app/modules/timeline/timeline.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from "@angular/core";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatTableModule} from "@angular/material/table";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 const timeLineModule: Route[] = [
   {
@@ -16,12 +17,13 @@ const timeLineModule: Route[] = [
   declarations: [
     TimelineComponent,
   ],
-  imports: [
-    RouterModule.forChild(timeLineModule),
-    MatCardModule,
-    MatButtonModule,
-    MatTableModule,
-  ]
+    imports: [
+        RouterModule.forChild(timeLineModule),
+        MatCardModule,
+        MatButtonModule,
+        MatTableModule,
+        MatToolbarModule,
+    ]
 })
 
 export class TimelineModule
